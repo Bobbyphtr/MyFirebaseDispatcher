3c8e563585fbdff97680d4a8efa75690ccaff4da
Xenoire <xeonexenoire@gmail.com>
2018-12-15 18:31:59 +0800

    Initial commit

---
+++ app/src/main/java/com/xenoire/myfirebasedispatcher/MainActivity.java
@@ -0,0 +1,81 @@
+package com.xenoire.myfirebasedispatcher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.jobdispatcher.Constraint;
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
+import com.firebase.jobdispatcher.Lifetime;
+import com.firebase.jobdispatcher.RetryStrategy;
+import com.firebase.jobdispatcher.Trigger;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnSetScheduler, btnCancelScheduler;
+    final String CITY = "Denpasar";
+    private String DISPATCHER_TAG = "dispatcher";
+    private FirebaseJobDispatcher jobDispatcher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnCancelScheduler = findViewById(R.id.btn_cancel_scheduler);
+        btnSetScheduler = findViewById(R.id.btn_set_scheduler);
+
+        btnCancelScheduler.setOnClickListener(this);
+        btnSetScheduler.setOnClickListener(this);
+
+        jobDispatcher = new FirebaseJobDispatcher(new GooglePlayDriver(this));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_set_scheduler:
+                Toast.makeText(this, "Dispatcher Created", Toast.LENGTH_SHORT).show();
+                startDispatcher();
+                break;
+            case R.id.btn_cancel_scheduler:
+                Toast.makeText(this, "Dispatcher Cancelled", Toast.LENGTH_SHORT).show();
+                cancelDispatcher(DISPATCHER_TAG);
+                break;
+        }
+    }
+
+    public  void startDispatcher(){
+        Bundle myExtrasBundle = new Bundle();
+        myExtrasBundle.putString(MyJobService.EXTRA_CITY, CITY);
+
+        Job job = jobDispatcher.newJobBuilder()
+                .setService(MyJobService.class) // kelas service
+                .setTag(DISPATCHER_TAG)         // Identifier unique untuk identifikasi
+                .setRecurring(true)             // TRUE jika job tersebut akan diulang, FALSE jika tidak di ulang
+                .setLifetime(Lifetime.UNTIL_NEXT_BOOT) //UNTIL_NEXT_BOOT => sampai boot selanjutnya; FOREVER => Berjalan meski setelah reboot
+                .setTrigger(Trigger.executionWindow(0, 60)) //waktu trigger
+                .setReplaceCurrent(true)        //TRUE untuk overwrite job dengan tag sama
+                .setRetryStrategy(RetryStrategy.DEFAULT_EXPONENTIAL) //waktu kapan akan dijalankan lagi jika gagal
+                .setConstraints(
+                        //Constraint.ON_UNMETERED_NETWORK, //Berjalan untuk koneksi yang unmetered (Wifi)
+                        //Constraint.DEVICE_CHARGING,     //Hanya berkalan ketika device di charge
+                        Constraint.ON_ANY_NETWORK      // Berjalan saat ada koneksi internet
+                        //Constraint.DEVICE_IDLE          //Berjalan saat device dalam kondisi idle
+                )
+                .setExtras(myExtrasBundle)
+                .build();
+        jobDispatcher.mustSchedule(job);
+        Log.e("Log e", "startDispatcher");
+    }
+
+    public void cancelDispatcher(String tag){
+        jobDispatcher.cancel(tag);
+        Log.e("Log e", "CancelDispatcher");
+    }
+}
---
+++ app/src/main/java/com/xenoire/myfirebasedispatcher/MyJobService.java
@@ -0,0 +1,106 @@
+package com.xenoire.myfirebasedispatcher;
+
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.media.RingtoneManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+import com.firebase.jobdispatcher.JobParameters;
+import com.firebase.jobdispatcher.JobService;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DecimalFormat;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MyJobService extends JobService {
+
+    public static final String TAG = MyJobService.class.getSimpleName();
+    final String WEATHER_API_KEY = "34b85c4702d2ef57c5cab3bb805f52c3";
+    public static String EXTRA_CITY = "city";
+    public final String NOTIFICATION_ID = "firebaseDispatcher";
+
+    @Override
+    public boolean onStartJob(JobParameters job) {
+        Log.d(TAG, "ONSTARTJOB : Executed");
+        getCurrentWeather(job);
+        return true;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters job) {
+        Log.d(TAG, "ONSTOPJOB : EXECUTED");
+        return true;
+    }
+
+    private void getCurrentWeather(final JobParameters job){
+
+        Bundle extras = job.getExtras();
+        // Lakukan pengecekan terlebih dahulu terhadap parameter job
+        if (extras == null) {
+            jobFinished(job, false);
+            return;
+        } else if (extras.isEmpty()) {
+            jobFinished(job, false);
+            return;
+        }
+        String city = extras.getString(EXTRA_CITY);
+        AsyncHttpClient client = new AsyncHttpClient();
+        String url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&APPID="+WEATHER_API_KEY;
+        client.get(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                String result = new String(responseBody);
+                Log.d(TAG, result);
+                try {
+                    JSONObject obj = new JSONObject(result);
+                    String currentWeather = obj.getJSONArray("weather").getJSONObject(0).getString("main");
+                    String description = obj.getJSONArray("weather").getJSONObject(0).getString("description");
+                    double tempInKelvin = obj.getJSONObject("main").getDouble("temp");
+                    double tempInCelcius = tempInKelvin - 273;
+                    String temperature = new DecimalFormat("##.##").format(tempInCelcius);
+                    String city = obj.getString("name");
+                    String title = city + " Current Weather";
+                    String message = currentWeather + ", " + description + " with "+temperature+" C";
+                    int notifId = 10;
+                    showNotification(getApplicationContext(), title, message, notifId);
+                    jobFinished(job, false);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    jobFinished(job, true);
+                }
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                jobFinished(job, true);
+            }
+        });
+    }
+     private void showNotification(Context context, String title, String msg, int notifId){
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel(NOTIFICATION_ID, "Weather Firebase Notification", NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setVibrationPattern(new long[]{0, 1000});
+            channel.enableVibration(true);
+        }
+         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, NOTIFICATION_ID)
+                 .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                 .setSmallIcon(R.drawable.ic_replay_30_black_24dp)
+                 .setContentTitle(title)
+                 .setContentText(msg)
+                 .setColor(ContextCompat.getColor(context, android.R.color.black));
+        notificationManager.notify(notifId, builder.build());
+     }
+}
